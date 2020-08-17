5e6428d7635ee49b96ac179d4893d68ff8a0cf55
EmersonPaduan <emerson.paduan@gmail.com>
2020-08-17 16:00:16 -0300

    aula 01 parte1

---
+++ aula01/exemplos/Exemplo01.java
@@ -0,0 +1,22 @@
+package exemplos; //pastas dentro do projeto
+
+/*
+Este é um comentário de bloco
+Serve para documentar partes do programa
+*/
+
+// Este é um comentári ode linha
+
+public class Exemplo01 {
+    public static void main(String[] args) {
+        //print não pula linha
+        //println pula linha
+        System.out.print("Meu ");
+        System.out.print("primeiro ");
+        System.out.println ("programa!");
+
+        //quanto utilizo sinal de + com textos, ele concatena (juntar)
+        System.out.println("Olá " + "como vai?");
+    }
+
+}
