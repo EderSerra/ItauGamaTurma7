de046dab8f4216eba5d040d4aaa1c00c010ab84a
EmersonPaduan <emerson.paduan@gmail.com>
2020-08-19 11:16:36 -0300

    aula 03 parte 1

---
+++ aula03/exemplos/Exemplo02.java
@@ -0,0 +1,26 @@
+package exemplos;
+
+import java.util.Scanner;
+
+public class Exemplo02 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int cont; //conta quantas notas serão digitadas
+        double nota, soma, media;
+        final int QTDENOTAS = 3;
+
+        soma = 0; //acumulador das notas digitadas
+        cont = 1;
+        while( cont <= QTDENOTAS ){
+            System.out.printf("Digite a nota (%d / %d): ", cont, QTDENOTAS);
+            nota = entrada.nextDouble();
+            soma = soma + nota;
+            cont++;
+        }
+
+        media = soma / QTDENOTAS;
+        System.out.printf("A média das notas é: %.2f\n", media);
+
+        entrada.close();
+    }
+}
